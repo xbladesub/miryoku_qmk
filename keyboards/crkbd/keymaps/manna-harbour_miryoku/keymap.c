f8cb440eec9dda2ea62edf542125d3a88c5e1cd7
Nikolai Shelekhov <nickshv13@icloud.com>
2022-09-25 13:26:33 +0800

    commit

--- keyboards/crkbd/keymaps/manna-harbour_miryoku/keymap.c
+++ keyboards/crkbd/keymaps/manna-harbour_miryoku/keymap.c
@@ -1,8 +1,8 @@
 // Copyright 2019 Manna Harbour
 // https://github.com/manna-harbour/miryoku
+// generated -*- buffer-read-only: t -*-
 
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
-
 
 #include QMK_KEYBOARD_H
 #include <stdio.h>
@@ -15,99 +15,77 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
   return rotation;
 }
 
-#define L_BASE 0
-#define L_LOWER 2
-#define L_RAISE 4
-#define L_ADJUST 8
+enum layers { BASE, BUTTON, MEDIA, NAV, MOUSE, SYM, NUM, FUN };
 
 void oled_render_layer_state(void) {
-    oled_write_P(PSTR("Layer: "), false);
-    switch (layer_state) {
-        case L_BASE:
-            oled_write_ln_P(PSTR("Default"), false);
-            break;
-        case L_LOWER:
-            oled_write_ln_P(PSTR("Lower"), false);
-            break;
-        case L_RAISE:
-            oled_write_ln_P(PSTR("Raise"), false);
-            break;
-        case L_ADJUST:
-        case L_ADJUST|L_LOWER:
-        case L_ADJUST|L_RAISE:
-        case L_ADJUST|L_LOWER|L_RAISE:
-            oled_write_ln_P(PSTR("Adjust"), false);
-            break;
-    }
-}
+  oled_write("Layer: ", false);
 
-
-char keylog_str[24] = {};
-
-const char code_to_name[60] = {
-    ' ', ' ', ' ', ' ', 'a', 'b', 'c', 'd', 'e', 'f',
-    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-    'R', 'E', 'B', 'T', '_', '-', '=', '[', ']', '\\',
-    '#', ';', '\'', '`', ',', '.', '/', ' ', ' ', ' '};
-
-void set_keylog(uint16_t keycode, keyrecord_t *record) {
-  char name = ' ';
-    if ((keycode >= QK_MOD_TAP && keycode <= QK_MOD_TAP_MAX) ||
-        (keycode >= QK_LAYER_TAP && keycode <= QK_LAYER_TAP_MAX)) { keycode = keycode & 0xFF; }
-  if (keycode < 60) {
-    name = code_to_name[keycode];
+  switch (get_highest_layer(layer_state)) {
+  case BASE:
+    oled_write("Base", false);
+    break;
+  case BUTTON:
+    oled_write("Button", false);
+    break;
+  case MEDIA:
+    oled_write("Media", false);
+    break;
+  case NAV:
+    oled_write("Navigation", false);
+    break;
+  case MOUSE:
+    oled_write("Mouse", false);
+    break;
+  case SYM:
+    oled_write("Symbol", false);
+    break;
+  case NUM:
+    oled_write("Number", false);
+    break;
+  case FUN:
+    oled_write("Function", false);
+    break;
   }
 
-  // update keylog
-  snprintf(keylog_str, sizeof(keylog_str), "%dx%d, k%2d : %c",
-           record->event.key.row, record->event.key.col,
-           keycode, name);
+  oled_write("\n", false);
 }
 
-void oled_render_keylog(void) {
-    oled_write(keylog_str, false);
+void oled_render_led_state(void) {
+  led_t led_state = host_keyboard_led_state();
+
+  oled_write(led_state.caps_lock ? "CAPS " : "      ", false);
+  oled_write(led_state.scroll_lock ? "SCROLL " : "       ", false);
+  oled_write("\n", false);
 }
 
-void render_bootmagic_status(bool status) {
-    /* Show Ctrl-Gui Swap options */
-    static const char PROGMEM logo[][2][3] = {
-        {{0x97, 0x98, 0}, {0xb7, 0xb8, 0}},
-        {{0x95, 0x96, 0}, {0xb5, 0xb6, 0}},
-    };
-    if (status) {
-        oled_write_ln_P(logo[0][0], false);
-        oled_write_ln_P(logo[0][1], false);
-    } else {
-        oled_write_ln_P(logo[1][0], false);
-        oled_write_ln_P(logo[1][1], false);
-    }
+void oled_render_mod_state(void) {
+  uint8_t mod_state = get_mods();
+
+  oled_write((mod_state & MOD_MASK_SHIFT) ? "SHIFT " : "      ", false);
+  oled_write((mod_state & MOD_MASK_CTRL) ? "CTRL " : "     ", false);
+  oled_write((mod_state & MOD_MASK_ALT) ? "ALT " : "    ", false);
+  oled_write((mod_state & MOD_MASK_GUI) ? "GUI" : "   ", false);
+  oled_write("\n", false);
 }
 
 void oled_render_logo(void) {
-    static const char PROGMEM crkbd_logo[] = {
-        0,  0,  0,  0,  0,  0,  0,  0,208, 32, 32, 32, 32, 32, 32, 32, 32, 32, 64,192,  0,  0,  0,  0,  0,  0,  0,224, 96,192,  0,  0,  0,  0,  0,  0,  4,  4,  4,  4,  4,244,  4,  4,  4, 12,  8,  8,  0,  0,  0,128,192, 64, 64, 64, 96, 32, 32, 32, 32, 32,  0,  0,  0,  0,  0,  0,224, 16, 16, 16, 16, 16, 16, 16, 16, 16,  0,  0,  0,  0,  0,  0, 24, 48, 96,192,128,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,128,240,  0,  0,  0,  0,  0,  0,  0,240, 24, 12,  4,  4, 28,240, 56, 12,  4,196,124,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,  0,240, 31,  0,  0,  0,  0,  0,  0,  0,  0,128,192, 97, 63,  0,  0,  0,192,120,142,129,128,129,135,156,176, 96,128,  0,  0,  0,  0,  0,  0,255,  0,  0,  0,  0,  0,  0,  0,  0,  7, 13, 24, 16, 16, 16, 16, 48, 32, 32, 32, 96,192,  0,  0,  0,  0,  0,255,  8,  8,  8,  8,  8,  8,  8,  8,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  3,  6, 12, 24,144,192, 96, 48, 12,  7,  1,  0,  0,  0,  0,  0,  0,  0,  0,  7, 24,112,192,  0,  0,192, 48, 28,  6,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 
-        0,  0,  0, 31, 32, 48, 16, 24,  8, 12,  4,  6,  3,  1,  0,  0,  0,  0,  0, 32, 63,  0,  0,  0,  0,  0,  0,  0,  0,  0,  7, 12,  0, 16, 16, 16, 16, 31, 16, 16, 16, 16,  0,  0,  0,  0,  8, 24, 48, 32, 32, 32, 32, 32, 32, 32, 16, 24, 15,  0,  0,  0,  0,  0, 23,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 32, 48, 28,  6,  3,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  3,  3,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-    };
-    oled_write_P(crkbd_logo, false);
+  static const char PROGMEM crkbd_logo[] = {
+    0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94,
+    0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4,
+    0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4,
+    0};
+  oled_write_P(crkbd_logo, false);
 }
 
 bool oled_task_user(void) {
-    if (is_keyboard_master()) {
-        oled_render_layer_state();
-        oled_render_keylog();
-    } else {
-        oled_render_logo();
-    }
-    return false;
+  if (is_keyboard_master()) {
+    oled_render_layer_state();
+    oled_render_mod_state();
+    oled_render_led_state();
+  } else {
+    oled_render_logo();
+  }
+  return false;
 }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
-    set_keylog(keycode, record);
-  }
-  return true;
-}
 #endif // OLED_ENABLE
